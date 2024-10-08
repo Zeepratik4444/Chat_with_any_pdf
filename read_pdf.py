306ee5666f84540d0d3128c17b5d620309bfaa89
Pratik Kumar <137696190+Zeepratik4444@users.noreply.github.com>
2024-10-08 14:29:28 +0530

    Add files via upload

---
+++ read_pdf.py
@@ -0,0 +1,60 @@
+import streamlit as st
+import os
+from langchain_groq import ChatGroq
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_community.embeddings import OllamaEmbeddings
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_core.prompts import ChatPromptTemplate
+from langchain.chains import create_retrieval_chain
+from langchain_community.vectorstores import FAISS
+from langchain_community.document_loaders import PyPDFDirectoryLoader
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+import google.generativeai as genai
+from dotenv import load_dotenv
+load_dotenv()
+## load the GROQ API Key
+os.environ['GROQ_API_KEY']=os.getenv("GROQ_API_KEY")
+os.getenv("GOOGLE_API_KEY")
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+
+groq_api_key=os.getenv("GROQ_API_KEY")
+
+llm=ChatGroq(groq_api_key=groq_api_key,model_name="Llama3-8b-8192")
+
+
+groq_api_key=os.getenv("GROQ_API_KEY")
+
+llm=ChatGroq(groq_api_key=groq_api_key,model_name="Llama3-8b-8192")
+
+prompt=ChatPromptTemplate.from_template(
+    """
+    Answer the questions based on the provided context only.
+    Please provide the most accurate respone based on the question
+    <context>
+    {context}
+    <context>
+    Question:{input}
+
+    """
+
+)
+
+embeddings=GoogleGenerativeAIEmbeddings(model = "models/embedding-001")
+def EmbedPDF(dir):
+    #dir=f"r{dir}"
+    loader=PyPDFDirectoryLoader(dir)
+    docs=loader.load()
+    text_splitter=RecursiveCharacterTextSplitter(chunk_size=1000,chunk_overlap=200)
+    final_docs=text_splitter.split_documents(docs)
+    vectorsDB=FAISS.from_documents(final_docs,embeddings)
+    retriever=vectorsDB.as_retriever()
+    return retriever
+
+
+def QueryPDF(query:str,retriever):
+    document_chain=create_stuff_documents_chain(llm,prompt)
+    retrieval_chain=create_retrieval_chain(retriever,document_chain)
+    inpt= query
+    response=retrieval_chain.invoke({'input':inpt})
+    print(response['answer'])    

f4fed7e9cf5ae2180c1bcbe0398b79597fc30dca
Pratik Kumar <137696190+Zeepratik4444@users.noreply.github.com>
2024-10-08 16:42:48 +0530

    Update read_pdf.py

--- read_pdf.py
+++ read_pdf.py
@@ -1,8 +1,6 @@
-import streamlit as st
 import os
 from langchain_groq import ChatGroq
 from langchain_google_genai import ChatGoogleGenerativeAI
-from langchain_community.embeddings import OllamaEmbeddings
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_core.prompts import ChatPromptTemplate

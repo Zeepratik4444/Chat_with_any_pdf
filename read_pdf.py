1ddc12512e2312be9da88ae7188c3775e0053463
Pratik Kumar <137696190+Zeepratik4444@users.noreply.github.com>
2025-04-01 20:48:30 +0530

    Update read_pdf.py

--- read_pdf.py
+++ read_pdf.py
@@ -15,12 +15,6 @@ load_dotenv()
 os.environ['GROQ_API_KEY']=os.getenv("GROQ_API_KEY")
 os.getenv("GOOGLE_API_KEY")
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
-
-groq_api_key=os.getenv("GROQ_API_KEY")
-
-llm=ChatGroq(groq_api_key=groq_api_key,model_name="Llama3-8b-8192")
-
-
 groq_api_key=os.getenv("GROQ_API_KEY")
 
 llm=ChatGroq(groq_api_key=groq_api_key,model_name="Llama3-8b-8192")
